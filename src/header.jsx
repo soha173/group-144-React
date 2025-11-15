0ce711869f74c44a653cc46296fd0b3259c08d3f
soha173 <sohamohammedbusiness@gmail.com>
2025-11-15 20:42:19 +0200

    last-version

---
+++ src/header.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+export function Header() {
+    const navigate = useNavigate();
+    function goToHome() {
+        navigate('/home')
+    }
+
+    return (
+        <div>
+            <h1>This is Header</h1>
+            <h1>New Text</h1>
+            <button onClick={goToHome}>Home</button>
+        </div>
+    )
+}
